36cc9bf02c51a0b959a36939d69500038c150a1b
pablo <pnunfe@gmail.com>
2024-10-01 19:09:17 +0200

    Refactor UserServiceApi to remove unnecessary tap operator and fix baseUrl typo

--- libs/shared/data-access/user-service/src/lib/api/userService-api.ts
+++ libs/shared/data-access/user-service/src/lib/api/userService-api.ts
@@ -1,19 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { SingleUserResponse, User } from '../models/userService.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserServiceApi {
-  readonly baseUrl = 'https//reqres.in/api/users';
+  readonly baseUrl = 'https://reqres.in/api/users';
   readonly http = inject(HttpClient);
 
   getUsersById(id: number): Observable<User> {
-    return this.http.get<SingleUserResponse>(`${this.baseUrl}/${id}`).pipe(
-      map((response) => response.data),
-      tap((user) => console.log(user))
-    );
+    return this.http
+      .get<SingleUserResponse>(`${this.baseUrl}/${id}`)
+      .pipe(map((response) => response.data));
   }
 }
--- libs/signalsnx/feature-user-info/src/lib/signalsnx-feature-user-info/signalsnx-feature-user-info.component.ts
+++ libs/signalsnx/feature-user-info/src/lib/signalsnx-feature-user-info/signalsnx-feature-user-info.component.ts
@@ -2,8 +2,12 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  inject,
+  OnInit,
+  signal,
   ViewEncapsulation,
 } from '@angular/core';
+import { User, UserServiceApi } from '@signals-nx/userService';
 
 @Component({
   selector: 'lib-signalsnx-feature-user-info',
@@ -14,19 +18,23 @@ import {
     <hr />
     <div class="row">
       <div class="col-4">
-        <h3>Usuario Actual:</h3>
-        <button class="btn btn-primary m-2">Anterior</button>
-        <button class="btn btn-primary m-2">Siguiente</button>
+        <h3>Usuario Actual: {{ userId() }}</h3>
+        <button class="btn btn-primary m-2" (click)="loadUser(userId() - 1)">
+          Anterior
+        </button>
+        <button class="btn btn-primary m-2" (click)="loadUser(userId() + 1)">
+          Siguiente
+        </button>
       </div>
       <div class="col-8">
-        <h3>Usuario:</h3>
-        <p>Nombre:</p>
-        <p>Correo:</p>
-
-        <img src="..." alt="avatar" />
-      </div>
-      <div class="col-8 text-danger">
+        @if (currentUser()) {
+        <h3>Usuario</h3>
+        <p>Nombre: {{ currentUser()!.first_name }}</p>
+        <p>Correo: {{ currentUser()!.email }}</p>
+        <img [src]="currentUser()!.avatar" alt="avatar" class="rounded" />
+        } @else {
         <h3>Usuario no encontrado</h3>
+        }
       </div>
     </div>
   `,
@@ -34,4 +42,23 @@ import {
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignalsnxFeatureUserInfoComponent {}
+export class SignalsnxFeatureUserInfoComponent implements OnInit {
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+  readonly userService = inject(UserServiceApi);
+  public userId = signal(1);
+
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+
+  loadUser(id: number) {
+    if (id <= 0) return; // No se puede cargar un usuario con id <= 0
+    this.userId.set(id);
+
+    this.userService.getUsersById(id).subscribe((user) => {
+      this.currentUser.set(user);
+      this.userWasFound.set(true);
+    });
+  }
+}
