942916b58198c0b105ee740ddf7296060e3a6c5b
pablo <pnunfe@gmail.com>
2024-10-02 17:34:15 +0200

    Refactor SignalsnxFeaturePropertiesComponent to add mutation inputs and logging

--- libs/signalsnx/feature-properties/src/lib/signalsnx-feature-properties/signalsnx-feature-properties.component.ts
+++ libs/signalsnx/feature-properties/src/lib/signalsnx-feature-properties/signalsnx-feature-properties.component.ts
@@ -2,8 +2,11 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
+  signal,
   ViewEncapsulation,
 } from '@angular/core';
+import { User } from '@signals-nx/userService';
 
 @Component({
   selector: 'lib-signalsnx-feature-properties',
@@ -18,22 +21,25 @@ import {
           type="text"
           class="form-control mb-2"
           (input)="onFieldUpdate('email', txtEmail.value)"
+          [value]="user().email"
         />
         <input
           #txtFirstName
           type="text"
           class="form-control mb-2"
           (input)="onFieldUpdate('first_name', txtFirstName.value)"
+          [value]="user().first_name"
         />
         <input
           #txtLastName
           type="text"
           class="form-control mb-2"
           (input)="onFieldUpdate('last_name', txtLastName.value)"
+          [value]="user().last_name"
         />
       </div>
       <div class="col-6">
-        <pre>{{ 'etiqueta' }}</pre>
+        <pre>{{ user() | json }}</pre>
       </div>
     </div> `,
   styles: ``,
@@ -41,7 +47,36 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SignalsnxFeaturePropertiesComponent {
-  onFieldUpdate(field: string, value: string) {
-    console.log(field, value);
+  public fullName = computed(() => {
+    return `${this.user().first_name} ${this.user().last_name}`;
+  });
+  public user = signal<User>({
+    id: 2,
+    email: 'janet.weaver@reqres.in',
+    first_name: 'Janet',
+    last_name: 'Weaver',
+    avatar: 'https://reqres.in/img/faces/2-image.jpg',
+  });
+
+  onFieldUpdate(field: keyof User, value: string) {
+    this.user.update((current) => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+      }
+      return current;
+    });
   }
 }
