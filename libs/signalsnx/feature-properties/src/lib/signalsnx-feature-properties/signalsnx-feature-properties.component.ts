67574ad7e34c10fad552f2524ad843453785a2dc
pablo <pnunfe@gmail.com>
2024-10-02 17:04:31 +0200

    Refactor SignalsnxFeaturePropertiesComponent to add mutation inputs and logging

--- libs/signalsnx/feature-properties/src/lib/signalsnx-feature-properties/signalsnx-feature-properties.component.ts
+++ libs/signalsnx/feature-properties/src/lib/signalsnx-feature-properties/signalsnx-feature-properties.component.ts
@@ -1,17 +1,47 @@
+import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   ViewEncapsulation,
 } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'lib-signalsnx-feature-properties',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>signalsnx-feature-properties works!</p>`,
+  template: `<h1>Mutaciones:</h1>
+    <hr />
+    <div class="row">
+      <div class="col-6">
+        <input
+          #txtEmail
+          type="text"
+          class="form-control mb-2"
+          (input)="onFieldUpdate('email', txtEmail.value)"
+        />
+        <input
+          #txtFirstName
+          type="text"
+          class="form-control mb-2"
+          (input)="onFieldUpdate('first_name', txtFirstName.value)"
+        />
+        <input
+          #txtLastName
+          type="text"
+          class="form-control mb-2"
+          (input)="onFieldUpdate('last_name', txtLastName.value)"
+        />
+      </div>
+      <div class="col-6">
+        <pre>{{ 'etiqueta' }}</pre>
+      </div>
+    </div> `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignalsnxFeaturePropertiesComponent {}
+export class SignalsnxFeaturePropertiesComponent {
+  onFieldUpdate(field: string, value: string) {
+    console.log(field, value);
+  }
+}
